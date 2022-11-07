073f894fb50db7b717bdec87eea84855df518293
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-11-07 13:50:28 +0200

    main

---
+++ js/code.js
@@ -0,0 +1,13 @@
+// Write to a document
+// document.write("Hello Deviant");
+// document.write("<br>My Name Is Nom De Guerre");
+// document.write("<br>Am a Developer Of Destruction");
+/*
+You are
+Awesome
+*/
+let userName = 'MatthewJesseThomas';
+let userAGE = 22;
+console.log(userName);
+console.log(userAGE);
+console.log(typeof userName);
