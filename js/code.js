c1046a54ddf44119d6d5eca7d0c4686137e8851e
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-11-08 10:51:24 +0200

    main

--- js/code.js
+++ js/code.js
@@ -6,8 +6,32 @@
 You are
 Awesome
 */
-let userName = 'MatthewJesseThomas';
-let userAGE = 22;
-console.log(userName);
-console.log(userAGE);
-console.log(typeof userName);
+// let userName = 'MatthewJesseThomas';
+// let userAGE = 22;
+// console.log(userName);
+// console.log(userAGE);
+// console.log(typeof userName);
+
+//Revision//
+// numb1 = 8; 
+// let useName = 'Deviant';
+// const vat = .16;
+// var surname = "Avernos";
+// var data;
+// console.log(data);
+// let numb1 = 'Deviant'+9+11;
+// console.log(numb1);
+//5
+// let numb1 = prompt("Please Enter numb1");
+//5
+// let numb2 = prompt("Please Enter numb2");
+// let addition = numb1 + numb2;
+// console.log(addition);
+// let numb1 = 4;
+// let numb2 = 3;
+// let product = eval (`${numb1}*${numb2}`);
+// console.log(product);
+// let firstName = "Matthew";
+// let age = 22;
+// console.log
+
